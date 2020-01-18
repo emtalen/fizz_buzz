7b5e528b0c36e355e9d91b6c376b95f81fff9b95
Emma_Thalen <thlalen.emma@gmail.com>
2020-01-18 14:34:57 +0100

    passing all tests and the method is working

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,13 @@
 
 def fizz_buzz(number)
-    number
+    if number %  15 == 0
+        'FizzBuzz!'
+    elsif number % 5 == 0
+        'Buzz!'
+    elsif number % 3 == 0
+        'Fizz!'
+    else
+        number
+    end
 end
+
