d67bc44b33959f9b11ad5059592b3c45d785c975
Emma_Thalen <thlalen.emma@gmail.com>
2020-01-18 14:56:19 +0100

    finishing up the code as course material says and runs the test again so everything is working, and it does

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,13 +1,24 @@
 
 def fizz_buzz(number)
-    if number %  15 == 0
+    if has_zero_remainder?(number, 15)
         'FizzBuzz!'
-    elsif number % 5 == 0
+    elsif has_zero_remainder?(number, 5)
         'Buzz!'
-    elsif number % 3 == 0
+    elsif has_zero_remainder?(number, 3)
         'Fizz!'
     else
         number
     end
 end
+
+def has_zero_remainder? (number, divider)
+    number % divider == 0
+end
+
+# 2.6.3 :004 > output = []
+#  => [] 
+# 2.6.3 :011 > 100.times {|n| output << fizz_buzz(n+1)}
+#  => 100 
+# 2.6.3 :012 > output
+#  => [1, 2, "Fizz!", 4, "Buzz!", "Fizz!", 7, 8, "Fizz!", "Buzz!", 11, "Fizz!", 13, 14, "FizzBuzz!", 16, 17, "Fizz!", 19, "Buzz!", "Fizz!", 22, 23, "Fizz!", "Buzz!", 26, "Fizz!", 28, 29, "FizzBuzz!", 31, 32, "Fizz!", 34, "Buzz!", "Fizz!", 37, 38, "Fizz!", "Buzz!", 41, "Fizz!", 43, 44, "FizzBuzz!", 46, 47, "Fizz!", 49, "Buzz!", "Fizz!", 52, 53, "Fizz!", "Buzz!", 56, "Fizz!", 58, 59, "FizzBuzz!", 61, 62, "Fizz!", 64, "Buzz!", "Fizz!", 67, 68, "Fizz!", "Buzz!", 71, "Fizz!", 73, 74, "FizzBuzz!", 76, 77, "Fizz!", 79, "Buzz!", "Fizz!", 82, 83, "Fizz!", "Buzz!", 86, "Fizz!", 88, 89, "FizzBuzz!", 91, 92, "Fizz!", 94, "Buzz!", "Fizz!", 97, 98, "Fizz!", "Buzz!"] 
 
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,13 +4,11 @@ describe 'fizz_buzz' do
 it "returns 'fizzbuzz' if number is divisible by 15" do
     expect(fizz_buzz(15)).to eq "FizzBuzz!"
     end
-
-it "returns 'fizz' if number is divisible by 3" do
-    expect(fizz_buzz(3)).to eq 'Fizz!'
-    end
-    
 it "returns 'buzz' if number is divisible by 5" do
     expect(fizz_buzz(5)).to eq "Buzz!"
+    end
+it "returns 'fizz' if number is divisible by 3" do
+    expect(fizz_buzz(3)).to eq 'Fizz!'
     end
 it 'return 1 if number is 1' do
     expect(fizz_buzz(1)).to eq 1
